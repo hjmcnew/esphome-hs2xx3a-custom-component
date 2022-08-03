d6955969319bc45ec1664e0bfebeb03826224e84
Herb McNew <github.com@herbmcnew.com>
2022-08-02 23:24:54 -0400

    Remove additional customization.

--- components/hs2xx3a/__init__.py
+++ components/hs2xx3a/__init__.py
@@ -14,6 +14,6 @@ CONFIG_SCHEMA = cv.Schema({
 }).extend(cv.COMPONENT_SCHEMA).extend(uart.UART_DEVICE_SCHEMA)
 
 def to_code(config):
-    var = cg.new_Pvariable(config[CONF_HS2XX3A_ID])
+    var = cg.new_Pvariable(config[CONF_ID])
     yield cg.register_component(var, config)
     yield uart.register_uart_device(var, config)
